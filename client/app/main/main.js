f847a49daadd2ef6ecd864acbc81935a74f3616e
Aladdin Al-Wattar <aladdin83@gmail.com>
2015-09-19 15:57:01 +0400

    added files to git

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('mcmsApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
---
+++ server/sqldb/index.js
@@ -0,0 +1,21 @@
+/**
+ * Sequelize initialization module
+ */
+
+'use strict';
+
+var path = require('path');
+var config = require('../config/environment');
+
+var Sequelize = require('sequelize');
+
+var db = {
+  Sequelize: Sequelize,
+  sequelize: new Sequelize(config.sequelize.uri, config.sequelize.options)
+};
+
+// Insert models below
+db.Thing = db.sequelize.import('../api/thing/thing.model');
+db.User = db.sequelize.import('../api/user/user.model');
+
+module.exports = db;
